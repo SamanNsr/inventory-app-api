eedc569b8f0467c99e7193f9953c19e7727077ee
Saman2000hp <1999saman@gmail.com>
2021-03-06 23:09:36 +0330

    seeding the database

---
+++ src/constants/ordered-table-names.ts
@@ -0,0 +1,17 @@
+import tableNames from './table-names';
+
+export default [
+  tableNames.related_item,
+  tableNames.item_image,
+  tableNames.item_info,
+  tableNames.item,
+  tableNames.size,
+  tableNames.manufacturer,
+  tableNames.address,
+  tableNames.user,
+  tableNames.item_type,
+  tableNames.country,
+  tableNames.city,
+  tableNames.shape,
+  tableNames.location,
+];
--- src/db/seeds/initial.ts
+++ src/db/seeds/initial.ts
@@ -1,13 +1,53 @@
-import * as Knex from "knex";
+import crypto from 'crypto';
+import * as Knex from 'knex';
+
+import orderedTableName from '../../constants/ordered-table-names';
+import tableNames from '../../constants/table-names';
+import { PasswordManager } from '../../services/password-manger';
 
 export async function seed(knex: Knex): Promise<void> {
-    // Deletes ALL existing entries
-    await knex("table_name").del();
+  // Deletes ALL existing entries
+  await orderedTableName.reduce(async (promise, table_name) => {
+    await promise;
+    console.log('Clearing', table_name);
 
-    // Inserts seed entries
-    await knex("table_name").insert([
-        { id: 1, colName: "rowValue1" },
-        { id: 2, colName: "rowValue2" },
-        { id: 3, colName: "rowValue3" }
-    ]);
-};
+    return knex(table_name).del();
+  }, Promise.resolve());
+
+  // Inserts seed entries
+  const password = crypto.randomBytes(8).toString('hex');
+
+  const user = {
+    email: '1999saman@gmail.com',
+    name: 'Saman',
+    password: await PasswordManager.toHash(password),
+  };
+
+  const [createdUser] = await knex(tableNames.user).insert(user).returning('*');
+
+  console.log(
+    'User created:',
+    {
+      password,
+    },
+    createdUser
+  );
+
+  await knex(tableNames.country).insert([
+    {
+      name: 'IR',
+    },
+    {
+      name: 'US',
+    },
+    {
+      name: 'UK',
+    },
+  ]);
+
+  await knex(tableNames.city).insert([
+    {
+      name: 'tehran',
+    },
+  ]);
+}
