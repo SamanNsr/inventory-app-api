eaceac53bc668cd8e0fd5ebdf3c7cd8eb183b1e4
Saman Nesari <saman.nesary@gmail.com>
2021-03-20 03:14:43 +0330

    make it better

--- src/model/item/model.ts
+++ src/model/item/model.ts
@@ -4,6 +4,9 @@ import tableNames from '../../constants/table-names';
 import schema from './schema.json';
 
 import { ItemInfo } from '../item_info/model';
+import { ItemType } from '../item_type/model';
+import { User } from '../user/model';
+import { ItemImage } from '../item_image/model';
 
 class Item extends Model {
   id!: number;
@@ -28,12 +31,36 @@ class Item extends Model {
 
   static get relationMappings() {
     return {
+      user: {
+        relation: Model.HasOneRelation,
+        modelClass: User,
+        join: {
+          from: `${tableNames.user}`,
+          to: `${tableNames.item}.user_id`,
+        },
+      },
       item_infos: {
         relation: Model.HasManyRelation,
         modelClass: ItemInfo,
         join: {
           from: `${tableNames.item}`,
           to: `${tableNames.item_info}.item_id`,
+        },
+      },
+      item_type: {
+        relation: Model.HasOneRelation,
+        modelClass: ItemType,
+        join: {
+          from: `${tableNames.item_type}`,
+          to: `${tableNames.item}.item_type_id`,
+        },
+      },
+      item_images: {
+        relation: Model.HasManyRelation,
+        modelClass: ItemImage,
+        join: {
+          from: `${tableNames.item}`,
+          to: `${tableNames.item_image}.item_id`,
         },
       },
     };
